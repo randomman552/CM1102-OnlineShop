f857fabcfb4ba54e2cdd882a2c2c77988947cc3b
George Grainger <GraingerGY@cardiff.ac.uk>
2020-03-03 00:39:01 +0000

    Basic layout created

--- shop/routes.py
+++ shop/routes.py
@@ -1,3 +1,7 @@
 #File to hold routes (we can split this into many separate files if it gets too big)
 from flask import render_template, redirect
 from . import app
+
+@app.route("/")
+def render():
+    return render_template("layout.html")
