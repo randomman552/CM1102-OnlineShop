a418230a5ada9f248cc08be9f1bb85ad8c84c25b
George Grainger <GraingerGY@cardiff.ac.uk>
2020-03-13 17:54:02 +0000

    Create basic structure

--- shop/routes.py
+++ shop/routes.py
@@ -5,3 +5,15 @@ from . import app
 @app.route("/")
 def render():
     return render_template("layout.html")
+
+@app.route("/products")
+def render_products():
+    return render_template("products/products.html")
+
+@app.route("/products/new", methods=["GET", "POST"])
+def render_new_product():
+    return render_template("products/new_product.html")
+
+@app.route("/products/<int:product_id>", methods=["GET", "POST"])
+def render_view_product(product_id):
+    return render_template("products/view_product.html")
