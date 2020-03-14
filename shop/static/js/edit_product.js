0ca0f4d09eac840806c5d323addb7b29f5389a23
George Grainger <GraingerGY@cardiff.ac.uk>
2020-03-14 18:52:42 +0000

    Add edit buttons

---
+++ shop/static/js/edit_product.js
@@ -0,0 +1,36 @@
+//Functions for changing product descriptions
+function openDescriptionEdit() {
+	console.log('Description Edit Started');
+}
+function closeDescriptionEdit() {
+	console.log('Description Edit Ended');
+}
+
+//Functions for changing product names
+function openNameEdit() {
+	console.log('Name Edit Started');
+}
+function closeNameEdit() {
+	console.log('Name Edit Ended');
+}
+
+//Functions for adding images
+function openImageEdit() {
+	console.log('Image Edit Started');
+}
+function closeImageEdit() {
+	console.log('Image Edit Ended');
+}
+
+//Function for editing image order
+function openImageOrderEdit() {
+	console.log('Image Order Edit Started');
+}
+function closeImageOrderEdit() {
+	console.log('Image Order Edit Ended');
+}
+
+//Functions for changing state between public and private
+function setPublic(new_state) {
+	console.log(new_state);
+}
