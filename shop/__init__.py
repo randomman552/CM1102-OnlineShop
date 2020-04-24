599dbb14b24de322cd7e640767dda29486bfe3af
Cyril Smetanka <smetankac@cardiff.ac.uk>
2020-04-24 14:36:14 +0100

    Update __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,5 +1,7 @@
 #Any code we want to run before creating any routes or models should be put in here
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 import os
 
 app = Flask(__name__)
@@ -9,6 +11,15 @@ UPLOAD_FOLDER = 'static/products/temp'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
 
 from . import models
 from . import routes
+
+from flask_admin import Admin
+from shop.views import AdminView
+from flask_admin.contrib.sqla import ModelView
+from shop.models import Picture
+from shop.models import Product
+admin = Admin(app, name='Admin panel', template_mode='bootstrap3')
+admin.add_view(ModelView(Product, db.session))
