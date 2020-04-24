0ec168fb7fbcdad8896f9c23a51a78d97de822d7
George Grainger <GraingerGY@cardiff.ac.uk>
2020-04-24 12:19:28 +0100

    Add error feedback to reviews form

--- shop/forms.py
+++ shop/forms.py
@@ -6,5 +6,6 @@ from wtforms import validators
 class AddReviewForm(FlaskForm):
     stars = RadioField("Stars: ", choices=[
                        ("1", '1 Star'), ("2", '2 Star'), ("3", '3 Star'), ("4", '4 Star'), ("5", '5 Star')])
-    comment = TextAreaField("Comment: ")
+    comment = TextAreaField("Comment: ", validators=[
+                            validators.length(max=140, message="Your comment must be a maximum of 140 characers long.")])
     submit = SubmitField("Add review")
