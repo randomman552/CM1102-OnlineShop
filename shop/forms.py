c7aafd47cf6a4e6723269c03ed2284f3303531a1
Rachael Hanson <hansonr2@cardiff.ac.uk>
2020-04-27 18:56:42 +0100

    update checkout

--- shop/forms.py
+++ shop/forms.py
@@ -6,6 +6,7 @@ class ShippingForm(FlaskForm):
     firstname = StringField('First Name', validators=[validators.DataRequired(message='Please fill in your first name'), validators.Length(min=2, max=15, message='Last name must be at least 2 characters long')])
     lastname = StringField('Last Name', validators=[validators.DataRequired(message='Please fill in your last name'), validators.Length(min=2, max=15, message='Last name must be at least 2 characters long')])
     email = StringField('Email', validators=[validators.DataRequired(message='Please fill in your email address'), validators.Email(message='Invalid email address')])
+    phone = IntegerField('Phone', validators=[validators.DataRequired(message='Please type an 11 digit phone number')])
     address1 = TextAreaField('Address 1', validators=[validators.DataRequired(message='Please fill in the first line of your address'), validators.Length(min=3, max=30, message='Please type an address between 3 and 30 characters long')])
     address2 = TextAreaField('Address 2 ', validators=[validators.DataRequired(message='Please fill in the second line of your address'), validators.Length(min=3, max=30, message='Please type an address between 3 and 30 characters long')])
     postcode = StringField('Postcode ', validators=[validators.DataRequired(message='Please fill in your postcode'), validators.Length(min=6, max=9, message='Your postcode must be between 6 and 8 characters long')])
@@ -19,7 +20,7 @@ class BillingForm(FlaskForm):
    cardnumber4 = IntegerField('Card Number', validators=[validators.DataRequired(message='Type your 16 digit card number'), validators.NumberRange(min=0, max=9999, message='Type your 16 digit card number')])
    expirydate = SelectField('Expiry Date', choices=[('2020' , '2020'), ('2021' , '2021'), ('2022' , '2022'), ('2023' , '2023'), ('2024' , '2024'), ('2025' , '2025'), ('2026' , '2026'), ('2027' , '2027'), ('2028' , '2028'), ('2029' , '2029'), ('2030' , '2030'), ('2031' , '2031'), ('2032' , '2032'), ('2033' , '2033'), ('2034' , '2034'), ('2035' , '2035')])
    expirymonth = SelectField('Expiry month', choices=[('01' , '01'), ('02' , '02'), ('03' , '03'), ('04' , '04'), ('05' , '05'), ('06' , '06'), ('07' , '07'), ('08' , '08'), ('09' , '09'), ('10' , '10'), ('11' , '11'), ('12' , '12')])
-   cvv = StringField('CVV', validators=[validators.DataRequired(message='These are the three digits on the back'), validators.Length(min=3, max=3, message='These are the three digits on the back')])
+   cvv = IntegerField('CVV', validators=[validators.DataRequired(message='These are the three digits on the back'), validators.NumberRange(min=0, max=999, message='These are the three digits on the back')])
    submit2 = SubmitField('Proceed to Review')
 
 class ReviewForm(FlaskForm):
