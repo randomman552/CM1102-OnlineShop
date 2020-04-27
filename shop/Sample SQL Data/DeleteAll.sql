1aa5c01d573d6bdc794c5aa44a91d11864576ddc
George Grainger <GraingerGY@cardiff.ac.uk>
2020-04-27 21:21:02 +0100

    Replace SQL sample data with the new system

--- shop/Sample SQL Data/DeleteAll.sql
+++
@@ -1,7 +0,0 @@
-delete from `review` where `ID` > 0;
-delete from `wishlist` where `ID` > 0;
-delete from `user` where `ID` > 0;
-delete from `picture` where `ID` > 0;
-delete from `product_category` where `ID` > 0;
-delete from `category` where `ID` > 0;
-delete from `product` where `ID` > 0;
